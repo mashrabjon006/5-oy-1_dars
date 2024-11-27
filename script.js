78774b9f7b8f9dfeba623d1ed17aeb7440eab54c
Akramov Mashrabjon <mashrabjonakramov258@gmail.com>
2024-11-27 16:07:54 +0500

    JavaScript

---
+++ script.js
@@ -0,0 +1,37 @@
+// obyektlardan, massivlardan nusxa olish
+
+// shallow
+// deep copy
+
+//shalow
+// let user = {
+//     name:'john',
+//     age:'23'
+// }
+
+// let user1 = {...user}; //spread
+// user.age = 100;
+// console.log(user)
+
+
+// object assign
+// let user1 = object.assign({},user)
+
+// strinfy
+// let user1 = JSON.parse(JSON.stringify(user))
+// function sum(b){
+//     return a+b
+// }
+
+// console.log(sum(5))
+
+//  Ctrl+K+Ctrl+S
+
+// let i = 0
+// let timeout = setTimeout(()=>{
+//     i++
+//     console.log(i)
+//     if(i == 5){
+//         clearInterval(timeout)
+//     }
+// },1000)
